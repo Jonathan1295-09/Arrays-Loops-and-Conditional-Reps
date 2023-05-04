fa131312dad0377624f964150098056ba2d6452e
Jonathan1295-09 <jcasasola955@gmail.com>
2023-05-03 23:29:21 -0500

    "Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -9,3 +9,21 @@ for ( var i = 0; i < 200; i++){
         console.log(i)
     }
 }
+
+///////////
+
+for ( var i = 1; i <100; i++) {
+    var three = i % 3;
+    var five = i % 5;
+    if ( (three == 0) && (five == 0) )
+    console.log("FizzBuzz");
+
+    else if ( three == 0 )
+    console.log("Fizz");
+
+    else if ( five == 0)
+    console.log("Buzz");
+
+    else
+    console.log(i)
+}
