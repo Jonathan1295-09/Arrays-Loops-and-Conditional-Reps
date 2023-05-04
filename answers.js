a6bfe0190d94d9e27f7d73fb745f2fd353f876bb
Jonathan1295-09 <jcasasola955@gmail.com>
2023-05-04 00:19:36 -0500

    "Wild Wild Life answered"

--- answers.js
+++ answers.js
@@ -27,3 +27,27 @@ for ( var i = 1; i <100; i++) {
     else
     console.log(i)
 }
+
+////////////
+//Wild Wild Life
+////////////
+{
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+}
+
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+ let plantee = 1 + 5000
+ console.log(plantee)
+ 
+ // Change Wolfy's hometown from "Yukon Territory" to "Gotham City"
+  let YukonTerritory = "Gothem City"
+  console.log(YukonTerritory)
+
+  // Give dart a second hometown by adding "Hawkins"
+  const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+  dart.push("Hawkins");{
+  console.log(dart)}
